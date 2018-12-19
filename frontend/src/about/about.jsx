f646c45347bf2ef50e7ce2ac3d561cd30a6e85f3
wassilva <wassilva@prestadores.amil.com.br>
2018-12-19 19:37:05 -0200

    first commit casa

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../templates/pageHeader'
+
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nos'></PageHeader>    
+
+        <h2>Nossa historia</h2>
+        <p>Sera lembrada</p>
+        <h2>Missao e Visao</h2>
+        <p>Allem do alcance</p>
+    </div>
+)
